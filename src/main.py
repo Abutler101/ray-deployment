fe6918f56c0664827e13f2cdc29404142334473b
alexis <alexis@turintech.ai>
2023-02-07 08:56:11 +0000

    fix: correct typos

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ from sep_file import generate_data
 
 
 def main():
-    ray.init(address="ray://0.0.0.0:10001")
+    ray.init(address="ray://0.0.0.0:10001", runtime_env={"pip": ["codecarbon"]})
     print("connected to ray cluster")
     df_ref = generate_data.remote()
     print(f"got Ref: {df_ref}")
+    df = ray.get(df_ref)
+    print(f"Got Df {df.head()}")
 
 
 if __name__ == '__main__':
--- src/sep_file.py
+++ src/sep_file.py
@@ -8,7 +8,14 @@ import pandas as pd
 @ray.remote
 def generate_data() -> pd.DataFrame:
     """An intentionally slow method that generates a dataframe"""
-    a = pd.DataFrame(["num", "letter", float])
+    a = pd.DataFrame({"num": [], "letter": [], "float": []})
     for _ in range(100_000):
-        a.append([np.random.randint(0, 100), np.random.choice(string.ascii_lowercase), np.random.random()])
+        a = a.append(
+            {
+                "num": np.random.randint(0, 100),
+                "letter": np.random.choice(list(string.ascii_lowercase)),
+                "float": np.random.random()
+            },
+            ignore_index=True
+        )
     return a
